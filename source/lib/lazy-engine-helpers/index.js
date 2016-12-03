bfdcc2c54ea9fadedc0bb9d1f24379dc6b16fc47
ierceg <ivan@softwaremarbles.com>
2016-12-03 17:56:54 -0300

    engine-helpers - Move Engine and DockerizedEngine from Stack repo

--- source/lib/lazy-engine-helpers/index.js
+++ source/lib/lazy-engine-helpers/index.js
@@ -3,5 +3,7 @@
 
 module.exports = {
     AdaptedAtomLinter: require('./lib/adapted-atom-linter'),
-    Logger: require('./lib/logger')
+    Logger: require('./lib/logger'),
+    Engine: require('./lib/engine'),
+    DockerizedEngine: require('./lib/dockerized-engine')
 };
