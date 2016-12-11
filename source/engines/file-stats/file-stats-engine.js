0d15b2a8b1579926ff9c1c252e638b79923bc410
ierceg <ivan@softwaremarbles.com>
2016-12-11 18:20:59 -0300

    file-stats-engine - Proof of concept for file/language statistics

---
+++ source/engines/file-stats/file-stats-engine.js
@@ -0,0 +1,82 @@
+
+'use strict';
+
+const _ = require('lodash');
+
+const EngineHelpers = require('@lazyass/engine-helpers');
+const EngineHttpServer = EngineHelpers.EngineHttpServer;
+
+const quoteUnquoteDatabase = {};
+
+class FileStatsEngine
+{
+    /**
+     * Analyzes the given file content for the given language and analysis configuration.
+     * @param {string} content Content of the source file requesting lazy to analyze.
+     * @param {string} clientPath Path of the source file requesting lazy to analyze.
+     * @param {string} language Language of the source file.
+     * @param {string} config Name of the configuration to use.
+     * @return {Promise} Promise resolving with results of the file analysis.
+     */
+    analyzeFile(content, clientPath, language, config) {
+        //  We use a promise as we get any exceptions wrapped up as failures.
+        return new Promise((resolve) => {
+            const fileKey = 'file-' + clientPath;
+            let fileStats = quoteUnquoteDatabase[fileKey];
+            if (_.isUndefined(fileStats)) {
+                fileStats = {
+                    analysisCounter: 0
+                };
+            }
+
+            ++fileStats.analysisCounter;
+            quoteUnquoteDatabase[fileKey] = fileStats;
+
+            const languageKey = 'language-' + language;
+            let languageStats = quoteUnquoteDatabase[languageKey];
+            if (_.isUndefined(languageStats)) {
+                languageStats = {
+                    analysisCounter: 0
+                };
+            }
+
+            ++languageStats.analysisCounter;
+            quoteUnquoteDatabase[languageKey] = languageStats;
+
+            logger.warn('quoteUnquoteDatabase', JSON.stringify(quoteUnquoteDatabase, null, 4));
+
+            resolve({
+                stats: fileStats
+            });
+        });
+    }
+}
+
+class EslintEngineHttpServer extends EngineHttpServer
+{
+    _bootEngine() {
+        return Promise.resolve(new FileStatsEngine());
+    }
+
+    _stopEngine() {
+        return Promise.resolve();
+    }
+}
+
+class Engine
+{
+    start() {
+        const port = process.env.PORT || 80;
+        this._server = new EslintEngineHttpServer(port);
+        return this._server.start();
+    }
+
+    stop() {
+        return this._server.stop()
+            .then(() => {
+                this._server = null;
+            });
+    }
+}
+
+module.exports = Engine;
