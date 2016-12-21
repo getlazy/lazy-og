fdd69f6cefe6055c44a59a1a8908592cb8f19a16
neboysa <nebojsa.lazic@startlabs.rs>
2016-12-21 09:45:58 +0100

    eslint-engine - fix tests

--- source/engines/eslint/test/test-eslint-http-server.js
+++ source/engines/eslint/test/test-eslint-http-server.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 const _ = require('lodash');
@@ -15,8 +14,7 @@ const ANALYZE_FILE_FIXTURE = [{
     params: {
         path: '/src/test.js',
         language: 'JavaScript',
-        content:
-`
+        content: `
 'use strict';
 
 var x = 0;
@@ -26,9 +24,11 @@ var x = 0;
         const warnings = results.warnings;
         assert.equal(warnings.length, 2);
         const warningsPerType = _.groupBy(warnings, (warning) => warning.type);
-        assert.equal(warningsPerType['Warning'].length, 2);
-        assert.equal(warnings[0].message, 'Unexpected var, use let or const instead.');
-        assert.equal(warnings[1].message, '\'x\' is assigned a value but never used.');
+
+        // We should get two errors: [no-var] and [no-unused-vars]
+        assert.equal(warningsPerType['Error'].length, 2);
+        assert(warnings[0].message.endsWith('Unexpected var, use let or const instead.'));
+        assert(warnings[1].message.endsWith('\'x\' is assigned a value but never used.'));
     },
     catch: ASSERT_FALSE
 }, {
@@ -36,8 +36,7 @@ var x = 0;
     params: {
         path: '/src/test.js',
         language: 'JavaScript',
-        content:
-`
+        content: `
 var x =
 `
     },
@@ -46,30 +45,30 @@ var x =
         assert.equal(warnings.length, 1);
         const warningsPerType = _.groupBy(warnings, (warning) => warning.type);
         assert.equal(warningsPerType['Error'].length, 1);
-        assert.equal(warnings[0].message, 'Parsing error: Unexpected token');
+        assert(warnings[0].message.endsWith('Parsing error: Unexpected token'));
     },
     catch: ASSERT_FALSE
 }];
 
-describe('EslintEngineHttpServer', function() {
+describe('EslintEngineHttpServer', function () {
     this.timeout(20000);
 
-    before(function() {
+    before(function () {
         return require('./bootstrap').start();
     });
 
-    after(function() {
+    after(function () {
         return require('./bootstrap').stop();
     });
 
-    describe('POST /file', function() {
+    describe('POST /file', function () {
         let onlyFixtures = _.filter(ANALYZE_FILE_FIXTURE, (fixture) => fixture.only);
         if (_.isEmpty(onlyFixtures)) {
             onlyFixtures = ANALYZE_FILE_FIXTURE;
         }
         _.each(onlyFixtures, (fixture) => {
             let params = fixture.params;
-            it(fixture.name, function() {
+            it(fixture.name, function () {
                 const requestParams = {
                     method: 'POST',
                     url: 'http://localhost/file',
@@ -85,24 +84,24 @@ describe('EslintEngineHttpServer', function() {
                 };
 
                 return new Promise((resolve, reject) => {
-                    request(requestParams, (err, response, body) => {
-                        if (err) {
-                            return reject(err);
-                        }
-
-                        if (response.statusCode !== 200) {
-                            let message = 'HTTP engine failed with ' + response.statusCode +
-                                ' status code';
-                            if (body && body.error) {
-                                message += ' (' + body.error + ')';
+                        request(requestParams, (err, response, body) => {
+                            if (err) {
+                                return reject(err);
                             }
 
-                            return reject(new Error(message));
-                        }
+                            if (response.statusCode !== 200) {
+                                let message = 'HTTP engine failed with ' + response.statusCode +
+                                    ' status code';
+                                if (body && body.error) {
+                                    message += ' (' + body.error + ')';
+                                }
 
-                        resolve(body);
-                    });
-                })
+                                return reject(new Error(message));
+                            }
+
+                            resolve(body);
+                        });
+                    })
                     .then(fixture.then)
                     .catch(fixture.catch);
             });
