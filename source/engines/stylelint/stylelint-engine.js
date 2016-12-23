9e21f4c9bf684ad8db55d074b8b8f5605d35311b
ierceg <ivan@softwaremarbles.com>
2016-12-23 12:31:25 -0300

    stylelint-engine - Update to EngineHelpers 2
    Fix lazy warnings

--- source/engines/stylelint/stylelint-engine.js
+++ source/engines/stylelint/stylelint-engine.js
@@ -1,22 +1,26 @@
 
 'use strict';
 
+/* global logger */
+
 global.logger = require('@lazyass/engine-helpers').Logger.getEngineLogger();
 
 const _ = require('lodash');
-const selectn = require('selectn');
 
 const EngineHelpers = require('@lazyass/engine-helpers');
+const stylelint = require('stylelint');
+const stylelintConfigStandard = require('stylelint-config-standard');
+
 const EngineHttpServer = EngineHelpers.EngineHttpServer;
 
-//  TODO: Merge StylelintEngine, StylelintEngineHttpServer and Engine classes - their
-//  separation doesn't make much sense.
-
-class StylelintEngine
+//  We are implicitly using `this` in overriden methods but eslint keep telling us not to.
+/* eslint class-methods-use-this: off */
+class StylelintEngineHttpServer extends EngineHttpServer
 {
-    constructor() {
-        this._stylelint = require('stylelint');
-        this._stylelintConfigStandard = require('stylelint-config-standard');
+    beforeListening() {
+        this._stylelint = stylelint;
+        this._stylelintConfigStandard = stylelintConfigStandard;
+        return Promise.resolve();
     }
 
     /**
@@ -28,7 +32,7 @@ class StylelintEngine
      * @param {string} context Context information included with the request.
      * @return {Promise} Promise resolving with results of the file analysis.
      */
-    analyzeFile(hostPath, language, content, context) {
+    analyzeFile(hostPath, language, content/* , context*/) {
         const self = this;
 
         //  Convert lazy language to stylelint language.
@@ -42,41 +46,32 @@ class StylelintEngine
             code: content,
             config: self._stylelintConfigStandard
         })
-            .then((results) => {
-                return _
-                    .chain(selectn('results[0].warnings', results))
-                    .map((warning) => {
-                        try {
-                            return {
-                                type: _.capitalize(warning.severity),
-                                //  Remove the rule string from the final output.
-                                message: warning.text.replace(' (' + warning.rule + ')', ''),
-                                line: _.toNumber(warning.line),
-                                column: _.toNumber(warning.column)
-                            };
-                        } catch(e) {
-                            logger.error('Failed to process stylelint warning', warning);
-                        }
-                    })
-                    .filter()
-                    .value();
-            })
-            .then((warnings) => {
-                return {
-                    warnings: warnings
-                };
-            });
-    }
-}
-
-class StylelintEngineHttpServer extends EngineHttpServer
-{
-    _bootEngine() {
-        return Promise.resolve(new StylelintEngine());
+            .then(results => _
+                .chain(_.get(results, ('results[0].warnings')))
+                .map((warning) => {
+                    try {
+                        return {
+                            type: _.capitalize(warning.severity),
+                            //  Remove the rule string from the final output.
+                            message: warning.text,
+                            line: _.toNumber(warning.line),
+                            column: _.toNumber(warning.column)
+                        };
+                    } catch (e) {
+                        logger.error('Failed to process stylelint warning', warning);
+                        return null;
+                    }
+                })
+                .filter()
+                .value()
+            )
+            .then(warnings => ({ warnings }));
     }
 
-    _stopEngine() {
-        return Promise.resolve();
+    getMeta() {
+        return {
+            languages: ['CSS', 'SCSS', 'LESS', 'SugarCSS']
+        };
     }
 }
 
