9e751fb912110e4add22d843146356e9efcb9185
ierceg <ivan@softwaremarbles.com>
2016-12-23 12:30:33 -0300

    file-stats-engine - Update to EngineHelpers 2
    Fix lazy warnings

--- source/engines/file-stats/file-stats-engine.js
+++ source/engines/file-stats/file-stats-engine.js
@@ -3,74 +3,40 @@
 
 const _ = require('lodash');
 const low = require('lowdb');
+//  lowdb uses lodash internally but eslint cannot know that.
+/* eslint lodash/prefer-lodash-method: off */
+const LowdbLibFileAsync = require('lowdb/lib/file-async');
 const mkdirp = require('mkdirp');
-
 const EngineHelpers = require('@lazyass/engine-helpers');
+
 const EngineHttpServer = EngineHelpers.EngineHttpServer;
 
 const LAZY_ENGINE_SANDBOX_DIR = process.env.LAZY_ENGINE_SANDBOX_DIR;
 
-//  Create directory for our json database.
-mkdirp.sync(LAZY_ENGINE_SANDBOX_DIR);
-
-const db = low(LAZY_ENGINE_SANDBOX_DIR + '/stats.json', {
-    storage: require('lowdb/lib/file-async')
-});
-
-//  Create the schema.
-db.defaults({
-    AnalyzeFileEvent: []
-})
-    .value();
-
-class FileStatsEngine
-{
-    /**
-     * Analyzes the given file content for the given language and analysis configuration.
-     * @param {string} host Name of the host requesting file analysis.
-     * @param {string} hostPath Path of the source file requesting lazy to analyze.
-     * @param {string} language Language of the source file.
-     * @param {string} content Content of the source file requesting lazy to analyze.
-     * @param {string} context Context information included with the request.
-     * @return {Promise} Promise resolving with results of the file analysis.
-     */
-    analyzeFile(hostPath, language, content, context) {
-        //  We use a promise as we get any exceptions wrapped up as failures.
-        return new Promise((resolve) => {
-            //  We capture the events and then later extract the stats on-demand.
-            db.get('AnalyzeFileEvent')
-                .push({
-                    time: Date.now(),
-                    host: context && context.host,
-                    hostPath: hostPath,
-                    language: language,
-                    client: context && context.client,
-                    originRepository: _
-                        .chain(context)
-                        .get('repositoryInformation.remotes')
-                        .find((remote) => {
-                            return remote && _.eq(remote.name, 'origin');
-                        })
-                        .get('refs.fetch')
-                        .value(),
-                    branch: _.get(context, 'repositoryInformation.status.current')
-                })
-                .value();
-
-            resolve({});
-        });
-    }
-}
-
+//  We are implicitly using `this` in overriden methods but eslint keep telling us not to.
+/* eslint class-methods-use-this: off */
 class FileStatsEngineHttpServer extends EngineHttpServer
 {
-    _bootEngine() {
-        return Promise.resolve(new FileStatsEngine());
+    beforeListening() {
+        //  Create directory for our json database.
+        mkdirp.sync(LAZY_ENGINE_SANDBOX_DIR);
+
+        this._db = low(`${LAZY_ENGINE_SANDBOX_DIR}/stats.json`, {
+            storage: LowdbLibFileAsync
+        });
+
+        //  Create the schema.
+        this._db.defaults({
+            AnalyzeFileEvent: []
+        })
+            .value();
+
+        return Promise.resolve();
     }
 
-    _customizeExpressApp(app) {
+    customizeExpressApp(app) {
         app.get('/stats', (req, res) => {
-            const stats = db.get('AnalyzeFileEvent')
+            const stats = this._db.get('AnalyzeFileEvent')
                 .reduce((stats, event) => {
                     ++stats.requests;
                     stats.requestsPerLanguage[event.language] =
@@ -100,8 +66,46 @@ class FileStatsEngineHttpServer extends EngineHttpServer
         });
     }
 
-    _stopEngine() {
-        return Promise.resolve();
+    /**
+     * Analyzes the given file content for the given language and analysis configuration.
+     * @param {string} host Name of the host requesting file analysis.
+     * @param {string} hostPath Path of the source file requesting lazy to analyze.
+     * @param {string} language Language of the source file.
+     * @param {string} content Content of the source file requesting lazy to analyze.
+     * @param {string} context Context information included with the request.
+     * @return {Promise} Promise resolving with results of the file analysis.
+     */
+    analyzeFile(hostPath, language, content, context) {
+        const self = this;
+
+        //  We use a promise as we get any exceptions wrapped up as failures.
+        return new Promise((resolve) => {
+            //  We capture the events and then later extract the stats on-demand.
+            self._db.get('AnalyzeFileEvent')
+                .push({
+                    time: Date.now(),
+                    host: context && context.host,
+                    hostPath,
+                    language,
+                    client: context && context.client,
+                    originRepository: _
+                        .chain(context)
+                        .get('repositoryInformation.remotes')
+                        .find(remote => remote && _.eq(remote.name, 'origin'))
+                        .get('refs.fetch')
+                        .value(),
+                    branch: _.get(context, 'repositoryInformation.status.current')
+                })
+                .value();
+
+            resolve({});
+        });
+    }
+
+    getMeta() {
+        return {
+            languages: []
+        };
     }
 }
 
