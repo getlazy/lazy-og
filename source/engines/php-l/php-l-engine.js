4f8a15b7044954dcf20a99023073922a04b46434
ierceg <ivan@softwaremarbles.com>
2016-12-23 12:31:01 -0300

    php-l-engine - Update to EngineHelpers 2
    Fix lazy warnings

--- source/engines/php-l/php-l-engine.js
+++ source/engines/php-l/php-l-engine.js
@@ -15,6 +15,8 @@ const LAZY_VOLUME_NAME = process.env.LAZY_VOLUME_NAME;
 
 const HELPER_CONTAINER_IMAGE_NAME = 'php:7.0.13-cli';
 
+//  We are implicitly using `this` in overriden methods but eslint keep telling us not to.
+/* eslint class-methods-use-this: off */
 class PhpLHelperContainer extends HelperContainer
 {
     _getContainerCmd() {
@@ -27,9 +29,9 @@ class PhpLHelperContainer extends HelperContainer
     _processContainerOutput(buffers) {
         //  Convert all the resulting buffers into string and join them as
         //  our parser works on a single string will all the output lines.
-        const output = _.map(buffers, (buffer) => {
-            return buffer && buffer.payload && buffer.payload.toString();
-        }).join('');
+        const output = _.map(buffers,
+            buffer => buffer && buffer.payload && buffer.payload.toString()
+        ).join('');
         const messages = [];
 
         //  As seen in https://github.com/AtomLinter/linter-php/blob/master/lib/main.js (MIT license)
@@ -58,17 +60,28 @@ class PhpLHelperContainer extends HelperContainer
 
 class PhpLEngineHttpServer extends EngineHttpServer
 {
-    _bootEngine() {
+    beforeListening() {
         return HelperContainer
             .createContainer(REPOSITORY_AUTH, HELPER_CONTAINER_IMAGE_NAME, LAZY_VOLUME_NAME)
             .then((container) => {
                 //  Assume that the container has started correctly.
                 this._container = container;
-                return new PhpLHelperContainer(container);
+                this._engine = new PhpLHelperContainer(container);
             });
     }
 
-    _stopEngine() {
+    getMeta() {
+        return {
+            languages: ['PHP']
+        };
+    }
+
+    analyzeFile() {
+        //  Pass forward the arguments to the engine.
+        return this._engine.analyzeFile.apply(this._engine, arguments);
+    }
+
+    afterListening() {
         //  Prevent trying to stop the same container twice.
         const container = this._container;
         this._container = null;
