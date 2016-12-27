4a898a1db364fc370d1c53dff5b405df083d9653
neboysa <nebojsa.lazic@startlabs.rs>
2016-12-27 16:34:24 +0100

    yaml-engine - Initial YAML linter

---
+++ source/engines/yaml/yaml-engine.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const _ = require('lodash');
+const path = require('path');
+const yaml = require('js-yaml');
+const EngineHelpers = require('@lazyass/engine-helpers');
+
+const EngineHttpServer = EngineHelpers.EngineHttpServer;
+
+class YamlEngine {
+
+    _processMsg(type, message) {
+            return {
+                type: type,
+                message: message.reason,
+                line: message.mark.line,
+                column: message.mark.column
+            };
+
+        }
+
+    /**
+     * Analyzes the given YAML file content through js-yaml parser.
+     * @param {string} hostPath Path of the source file requesting lazy to analyze.
+     * @param {string} language Language of the source file.
+     * @param {string} content Content of the source file requesting lazy to analyze.
+     * @param {string} context Context information included with the request.
+     * @return {Promise} Promise resolving with results of the file analysis.
+     */
+    analyzeFile(hostPath, language, content, context) {
+        const self = this;
+        //  We use a promise as we get any exceptions wrapped up as failures.
+        return new Promise((resolve) => {
+            const warnings = [];
+
+            try {
+                yaml.safeLoad(content, () => ({}), {
+                    filename: path.basename(hostPath),
+                    onWarning: (warning) => {
+                        warnings.push(self._processMsg('Warning', warning));
+                    },
+                });
+            } catch (error) {
+                warnings.push(self._processMsg('Error', error));
+            }
+            resolve({
+                warnings: warnings
+            });
+        });
+    }
+}
+
+class YamlEngineHttpServer extends EngineHttpServer {
+    _bootEngine() {
+        return Promise.resolve(new YamlEngine());
+    }
+
+    _stopEngine() {
+        return Promise.resolve();
+    }
+}
+
+class Engine {
+    start() {
+        const port = process.env.PORT || 80;
+        this._server = new YamlEngineHttpServer(port);
+        return this._server.start();
+    }
+
+    stop() {
+        return this._server.stop()
+            .then(() => {
+                this._server = null;
+            });
+    }
+}
+
+module.exports = Engine;
