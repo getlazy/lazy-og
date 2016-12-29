56d856b3a05782db10a4dba51a87a580bd4c1d2c
neboysa <nebojsa.lazic@startlabs.rs>
2016-12-29 15:01:11 +0100

    pullreq-engine - Pull request linter engine

---
+++ source/engines/pullreq/index.js
@@ -0,0 +1,31 @@
+
+'use strict';
+
+const EngineHelpers = require('@lazyass/engine-helpers');
+global.logger = EngineHelpers.Logger.getEngineLogger();
+
+const Engine = require('./pr-engine');
+const engine = new Engine();
+
+engine.start()
+    .then(() => {
+        logger.info('engine started');
+    })
+    .catch((err) => {
+        logger.error('Failed to start', err);
+        process.exit(-1);
+    });
+
+//  Setup graceful termination on SIGTERM.
+process.on('SIGTERM', () => {
+    logger.info('Received SIGTERM, stopping engine.');
+    engine.stop()
+        .then(() => {
+            logger.info('Engine stopped.');
+            process.exit(0);
+        })
+        .catch((err) => {
+            logger.error('Error occurred during stopping', err);
+            process.exit(-1);
+        });
+});
---
+++ source/engines/pullreq/pr-engine.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const _ = require('lodash');
+const EngineHelpers = require('@lazyass/engine-helpers');
+
+const EngineHttpServer = EngineHelpers.EngineHttpServer;
+
+const RepoLinter = require('./app/repo-linter.js');
+
+
+class PullRequestEngine {
+
+    /**
+     * Get all PR comments and turn them into lint messages.
+     * @param {string} hostPath Path of the source file requesting lazy to analyze.
+     * @param {string} language Language of the source file.
+     * @param {string} content Content of the source file requesting lazy to analyze.
+     * @param {string} context Context information included with the request.
+     * @return {Promise} Promise resolving with results of the both file and PR analysis.
+     */
+    analyzeFile(hostPath, language, content, context) {
+        return new Promise((resolve) => {
+            const repoLinter = new RepoLinter();
+            repoLinter.lintRepo(context).then((prComments) => {
+                const warnings = _.reduce(prComments, (result, comment) => {
+                    return _.concat(result, {
+                        type: 'PR',
+                        message: `@${comment.reviewer}: ${comment.message}`,
+                        line: comment.line,
+                        column: 1,
+                        moreInfo: comment.url
+                    });
+                }, []);
+                resolve({
+                    warnings: warnings
+                });
+            });
+        });
+    }
+}
+
+class PREngineHttpServer extends EngineHttpServer {
+    _bootEngine() {
+        return Promise.resolve(new PullRequestEngine());
+    }
+
+    _stopEngine() {
+        return Promise.resolve();
+    }
+}
+
+class Engine {
+    start() {
+        const port = process.env.PORT || 80;
+        this._server = new PREngineHttpServer(port);
+        return this._server.start();
+    }
+
+    stop() {
+        return this._server.stop()
+            .then(() => {
+                this._server = null;
+            });
+    }
+}
+
+module.exports = Engine;
