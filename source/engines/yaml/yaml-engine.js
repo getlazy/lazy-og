3af844a0e8d930e55dee8c284a4963e08a8ba76e
ierceg <ivan@softwaremarbles.com>
2017-01-01 20:31:19 -0300

    yaml-engine - Fix lazy warnings

--- source/engines/yaml/yaml-engine.js
+++ source/engines/yaml/yaml-engine.js
@@ -1,33 +1,32 @@
 
 'use strict';
 
-const _ = require('lodash');
 const path = require('path');
 const yaml = require('js-yaml');
 const EngineHelpers = require('@lazyass/engine-helpers');
 
 const EngineHttpServer = EngineHelpers.EngineHttpServer;
 
+//  We are implicitly using `this` in overriden methods but lazy keep telling us not to.
+/* eslint class-methods-use-this: off */
 class YamlEngineHttpServer extends EngineHttpServer {
     _processMsg(type, message) {
-            return {
-                type: type,
-                message: message.reason,
-                line: message.mark.line,
-                column: message.mark.column
-            };
-
-        }
+        return {
+            type,
+            message: message.reason,
+            line: message.mark.line,
+            column: message.mark.column
+        };
+    }
 
     /**
      * Analyzes the given YAML file content through js-yaml parser.
      * @param {string} hostPath Path of the source file requesting lazy to analyze.
      * @param {string} language Language of the source file.
      * @param {string} content Content of the source file requesting lazy to analyze.
-     * @param {string} context Context information included with the request.
      * @return {Promise} Promise resolving with results of the file analysis.
      */
-    analyzeFile(hostPath, language, content, context) {
+    analyzeFile(hostPath, language, content) {
         const self = this;
         //  We use a promise as we get any exceptions wrapped up as failures.
         return new Promise((resolve) => {
@@ -38,13 +37,13 @@ class YamlEngineHttpServer extends EngineHttpServer {
                     filename: path.basename(hostPath),
                     onWarning: (warning) => {
                         warnings.push(self._processMsg('Warning', warning));
-                    },
+                    }
                 });
             } catch (error) {
                 warnings.push(self._processMsg('Error', error));
             }
             resolve({
-                warnings: warnings
+                warnings
             });
         });
     }
