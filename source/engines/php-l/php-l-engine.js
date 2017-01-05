648f7c516e6d3e9008dba1818a28e4426ea5a122
ierceg <ivan@softwaremarbles.com>
2017-01-04 21:13:20 -0300

    php-l-engine - Remove docker.sock access

--- source/engines/php-l/php-l-engine.js
+++ source/engines/php-l/php-l-engine.js
@@ -3,8 +3,8 @@
 
 const _ = require('lodash');
 const H = require('higher');
-
 const EngineHelpers = require('@lazyass/engine-helpers');
+
 const HelperContainer = EngineHelpers.HelperContainer;
 const EngineHttpServer = EngineHelpers.EngineHttpServer;
 
@@ -63,10 +63,10 @@ class PhpLEngineHttpServer extends EngineHttpServer
     beforeListening() {
         return HelperContainer
             .createContainer(REPOSITORY_AUTH, HELPER_CONTAINER_IMAGE_NAME, LAZY_VOLUME_NAME)
-            .then((container) => {
+            .then((containerId) => {
                 //  Assume that the container has started correctly.
-                this._container = container;
-                this._engine = new PhpLHelperContainer(container);
+                this._containerId = containerId;
+                this._helperContainer = new PhpLHelperContainer(containerId);
             });
     }
 
@@ -76,16 +76,22 @@ class PhpLEngineHttpServer extends EngineHttpServer
         };
     }
 
-    analyzeFile() {
+    analyzeFile(...args) {
         //  Pass forward the arguments to the engine.
-        return this._engine.analyzeFile.apply(this._engine, arguments);
+        return this._helperContainer.analyzeFile(...args);
     }
 
     afterListening() {
+        this._helperContainer = null;
+
         //  Prevent trying to stop the same container twice.
-        const container = this._container;
-        this._container = null;
-        return HelperContainer.deleteContainer(container);
+        if (this._containerId) {
+            const containerId = this._containerId;
+            this._containerId = null;
+            return HelperContainer.deleteContainer(containerId);
+        }
+
+        return Promise.resolve();
     }
 }
 
