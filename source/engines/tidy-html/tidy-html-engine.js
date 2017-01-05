de88c9d7731522c6f5c7ff9cb994bcdc7babe47a
ierceg <ivan@softwaremarbles.com>
2017-01-04 21:12:50 -0300

    tidy-html-engine - Remove docker.sock access

--- source/engines/tidy-html/tidy-html-engine.js
+++ source/engines/tidy-html/tidy-html-engine.js
@@ -12,6 +12,7 @@ const _ = require('lodash');
 const H = require('higher');
 
 const EngineHelpers = require('@lazyass/engine-helpers');
+
 const HelperContainer = EngineHelpers.HelperContainer;
 const EngineHttpServer = EngineHelpers.EngineHttpServer;
 
@@ -66,10 +67,10 @@ class TidyHtmlEngineHttpServer extends EngineHttpServer
     beforeListening() {
         return HelperContainer
             .createContainer(REPOSITORY_AUTH, HELPER_CONTAINER_IMAGE_NAME, LAZY_VOLUME_NAME)
-            .then((container) => {
+            .then((containerId) => {
                 //  Assume that the container has started correctly.
-                this._container = container;
-                this._engine = new TidyHtmlHelperContainer(container);
+                this._containerId = containerId;
+                this._helperContainer = new TidyHtmlHelperContainer(containerId);
             });
     }
 
@@ -79,16 +80,22 @@ class TidyHtmlEngineHttpServer extends EngineHttpServer
         };
     }
 
-    analyzeFile() {
+    analyzeFile(...args) {
         //  Pass forward the arguments to the engine.
-        return this._engine.analyzeFile.apply(this._engine, arguments);
+        return this._helperContainer.analyzeFile(...args);
     }
 
     afterListening() {
+        this._helperContainer = null;
+
         //  Prevent trying to stop the same container twice.
-        const container = this._container;
-        this._container = null;
-        return HelperContainer.deleteContainer(container);
+        if (this._containerId) {
+            const containerId = this._containerId;
+            this._containerId = null;
+            return HelperContainer.deleteContainer(containerId);
+        }
+
+        return Promise.resolve();
     }
 }
 
