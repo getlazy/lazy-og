078e3f693ac2ab4b8334a83d35eb88856b1f8be7
neboysa <nebojsa.lazic@startlabs.rs>
2017-01-14 17:34:41 +0100

    postprocessor-engine - make // lazy ignore w/ no args to remove all masgs from current line

--- source/engines/postprocessor/postprocessor-engine.js
+++ source/engines/postprocessor/postprocessor-engine.js
@@ -26,7 +26,7 @@ class PostProcEngineHttpServer extends EngineHttpServer {
      */
     _parseLine(line) {
         // lazy ignore-once no-useless-escape ; Linter is confused w/ regex
-        const regex = /(#|\/\*|\/\/)\W*lazy\s+(\S*)\s+([^;\*]*)\s*;*(.*)/g;
+        const regex = /(#|\/\*|\/\/)\W*lazy\s+(\S*)\s*([^;\*]*)\s*;*(.*)/g;
 
         const command = {
             commandStr: '',
@@ -61,17 +61,24 @@ class PostProcEngineHttpServer extends EngineHttpServer {
         const lines = _.split(content, '\n');
         const directives = {
             ignore: [],
-            ignore_once: []
+            ignore_once: [],
+            ignore_local: []
         };
 
         _.forEach(lines, (oneLine, lineNo) => {
             const command = self._parseLine(oneLine);
 
             if (_.eq(command.commandStr, 'ignore')) {
-                // lazy ignore-once lodash/prefer-map ; _.map with be more complicated in this case
-                _.forEach(command.args, (ruleToIgnore) => {
-                    directives.ignore.push(_.toLower(ruleToIgnore));
-                });
+                if (_.isEmpty(command.args)) {
+                    // if there are no args, then treat this as
+                    // "ignore all warnings at this line"
+                    directives.ignore_local.push(lineNo + 1);
+                } else {
+                    // lazy ignore-once lodash/prefer-map ; _.map with be more complicated in this case
+                    _.forEach(command.args, (ruleToIgnore) => {
+                        directives.ignore.push(_.toLower(ruleToIgnore));
+                    });
+                }
             }
 
             if (_.eq(command.commandStr, 'ignore-once')) {
@@ -104,6 +111,37 @@ class PostProcEngineHttpServer extends EngineHttpServer {
         return warningList;
     }
 
+    /**
+     * Remove all messages that should be ignored in current line, only
+     * @param {Object} warningList List of messages from which to remove warnings
+     * @param {Object} toRemove List of lines to remove warnings from
+     * @return {Object} Filtered list of messages
+     */
+    _removeLocalWarnings(warningList, toRemove) {
+        const processedLines = [];
+
+        _.pullAllWith(warningList, toRemove, (warning, directiveLine) => {
+            const warningLine = _.parseInt(_.get(warning, 'line', 0), 10);
+            if (_.eq(warningLine, directiveLine)) {
+                processedLines.push(directiveLine);
+                return true;
+            }
+            return false;
+        });
+        // If there are some lines that are reported for ignorance,
+        // but they haven't been reported, mark their ignorance as warning
+        // lazy ignore-once lodash/prefer-map ; As we are just adding more warnings, following is more readable.
+        _.forEach(_.difference(toRemove, processedLines), (line) => {
+            warningList.push({
+                type: 'Warning',
+                message: 'No rules violated at this line.',
+                ruleId: ' lazy-no-ignore-line ',
+                line,
+                column: 1
+            });
+        });
+        return warningList;
+    }
 
     /**
      * Remove all messages that should be ignored once (only for the first time they occurr)
@@ -172,6 +210,7 @@ class PostProcEngineHttpServer extends EngineHttpServer {
             const directives = self._getLazyDirectives(content);
 
             self._removeIgnoreOnceWarnings(filteredWarnings, directives.ignore_once);
+            self._removeLocalWarnings(filteredWarnings, directives.ignore_local);
             self._removeIgnoreWarnings(filteredWarnings, directives.ignore);
 
             if (_.size(filteredWarnings) < 1) {
