bb9ae67fbcf00e085c98d4b8ed8ac8c63fca32c6
neboysa <nebojsa.lazic@startlabs.rs>
2017-01-17 21:04:17 +0100

    npm-task-engine - additional initial commit

--- source/engines/npm-task/engine-taskrun.js
+++ source/engines/npm-task/engine-taskrun.js
@@ -1,8 +1,6 @@
 'use strict';
 
 /* global logger */
-// lazy ignore lodash/chaining
-// lazy ignore lodash/chain-style
 
 const _ = require('lodash');  // lazy ignore-once lodash/import-scope
 const EngineHelpers = require('@lazyass/engine-helpers');
@@ -85,7 +83,6 @@ class TaskRunnerEngineHttpServer extends EngineHttpServer {
                 return TaskRunnerEngineHttpServer._installTaskNPMs(taskConfig)
                     .then((installedTasks) => {
                         self._installedTasks = _.compact(installedTasks);
-console.log(self._installedTasks);                        
                         return Promise.resolve();
                     });
             })
@@ -95,33 +92,11 @@ console.log(self._installedTasks);
             });
     }
 
-    _executeTask(taskName, hostPath, language, content, context) {
+    analyzeFile(hostPath, language, content, context) {
+        const taskName = _.get(context, 'engineParams.run');
         const task = _.find(this._installedTasks, taskName);
         const taskObj = _.get(task, taskName);
         return taskObj.executeLazy(hostPath, language, content, context);
-    }
-
-    /**
-     * Analyzes the given file content for the given language and analysis configuration.
-     * @param {string} hostPath Path of the source file requesting lazy to analyze.
-     * @param {string} language Language of the source file.
-     * @param {string} content Content of the source file requesting lazy to analyze.
-     * @param {string} context Context information included with the request.
-     * @return {Promise} Promise resolving with results of the file analysis.
-     */
-    analyzeFile(hostPath, language, content, context) {
-        const self = this;
-        const newContext = _.cloneDeep(context) || {};
-        const listOfTasksToRun = _.get(context, 'engineParams.tasks');
-
-        // Treat both string (one task) and sequence of tasks in the same way
-        const tasksToRun = (_.isString(listOfTasksToRun)) ? [listOfTasksToRun] : listOfTasksToRun;
-
-        // Execute tasks sequentially
-        return _.reduce(tasksToRun, (prevTask, nextTask) => prevTask.then((prevTaskResults) => {
-            newContext.previousStepResults = prevTaskResults;
-            return self._executeTask(nextTask, hostPath, language, content, newContext);
-        }), Promise.resolve());
     }
 }
 
