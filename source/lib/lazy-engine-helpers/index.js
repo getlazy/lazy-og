41e0ac686017624bdbb533a778dac82e9d5c8a44
ierceg <ivan@softwaremarbles.com>
2017-01-19 10:56:00 -0300

    engine-helpers - Add default handlers for uncaught exception/rejection

--- source/lib/lazy-engine-helpers/index.js
+++ source/lib/lazy-engine-helpers/index.js
@@ -1,7 +1,31 @@
 
 'use strict';
 
-/* eslint global-require: off */
+// Setup uncaught exception handling first thing in the process.
+const safeLog = (level, message, meta, callback) => {
+    if (global.logger) {
+        global.logger.log(level, message, meta, callback);
+    } else {
+        // lazy ignore no-console ; we don't have a logger so nowhere else to log
+        console.log(JSON.stringify({ level, message, meta }));
+    }
+};
+
+process.on('uncaughtException', (err) => {
+    safeLog('error', 'Uncaught exception', { err }, () => {
+        process.exit(-1001);
+    });
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    if (_.isObject(reason)) {
+        throw reason;
+    }
+
+    throw new Error(`Unhandled promise rejection: ${reason}`);
+});
+
+// lazy ignore global-require
 module.exports = {
     AdaptedAtomLinter: require('./lib/adapted-atom-linter'),
     Logger: require('./lib/logger'),
