bb4cca57cf216417b9f1a2594f1ed233447e5860
ierceg <ivan@softwaremarbles.com>
2017-01-22 22:53:03 -0300

    yaml-engine - Support multistream YAML

--- source/engines/yaml/yaml-engine.js
+++ source/engines/yaml/yaml-engine.js
@@ -8,14 +8,21 @@ const EngineHelpers = require('@lazyass/engine-helpers');
 
 const EngineHttpServer = EngineHelpers.EngineHttpServer;
 
-//  We are implicitly using `this` in overridden methods but lazy keep telling us not to.
-/* eslint class-methods-use-this: off */
+// We are implicitly using `this` in overridden methods but lazy keep telling us not to.
+// lazy ignore class-methods-use-this
 class YamlEngineHttpServer extends EngineHttpServer {
     _processMsg(type, message) {
+        // When we don't get a concrete line we consider the warning to be applicable to the
+        // entire file (thus line is `undefined`)
+        let line = _.get(message, 'mark.line') + 1; // Add +1 to correct for 0- and 1- based coordinate systems.
+        if (_.isNaN(line)) {
+            line = undefined;
+        }
+
         return {
             type,
             message: message.reason,
-            line: _.get(message, 'mark.line'),
+            line,
             column: _.get(message, 'mark.column')
         };
     }
@@ -29,20 +36,23 @@ class YamlEngineHttpServer extends EngineHttpServer {
      */
     analyzeFile(hostPath, language, content) {
         const self = this;
-        //  We use a promise as we get any exceptions wrapped up as failures.
+
+        // We use a promise as we get any exceptions wrapped up as failures.
         return new Promise((resolve) => {
             const warnings = [];
 
             try {
-                yaml.safeLoad(content, () => ({}), {
+                yaml.safeLoadAll(content, () => ({}), {
                     filename: path.basename(hostPath),
                     onWarning: (warning) => {
-                        warnings.push(self._processMsg('Warning', warning));
+                        warnings.push(
+                            self._processMsg('Warning', warning));
                     }
                 });
             } catch (error) {
                 warnings.push(self._processMsg('Error', error));
             }
+
             resolve({
                 status: {
                     codeChecked: true
