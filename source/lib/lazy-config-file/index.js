1ebcd79a48e231c97ed954c799cd93fbb35ef361
ierceg <ivan@softwaremarbles.com>
2017-01-26 02:38:29 -0300

    lazy-config-file - Fix package exporting

--- source/lib/lazy-config-file/index.js
+++ source/lib/lazy-config-file/index.js
@@ -1,7 +1,5 @@
 
 'use strict';
 
-module.exports = {
-    LazyConfigFile: require('./lib/lazy-config-file'),
-    logger: require('@lazyass/common').createPackageLogger('lazy-config-file')
-};
+module.exports = require('./lib/lazy-config-file');
+module.exports.logger = require('@lazyass/common').createPackageLogger('lazy-config-file');
