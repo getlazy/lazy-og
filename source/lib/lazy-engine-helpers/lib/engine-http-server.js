cdd84a57bdf5300d83c0155584e285d54be61c49
ierceg <ivan@softwaremarbles.com>
2017-02-04 03:32:17 -0300

    engine-helpers - Fix error logging

--- source/lib/lazy-engine-helpers/lib/engine-http-server.js
+++ source/lib/lazy-engine-helpers/lib/engine-http-server.js
@@ -134,7 +134,10 @@ class EngineHttpServer {
                 this._isReady = true;
             })
             .catch((err) => {
-                logger.error('Server listen failed', { err, port: this._port });
+                logger.error('Server listen failed', {
+                    err: err && err.toString(),
+                    port: this._port
+                });
                 //  We have to cleanup as we have already invoked `beforeListening`.
                 return this.afterListening()
                     //  Note that in this case it makes sense to define functions for
