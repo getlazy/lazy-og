14d6eae1c44e5e5f20af7ecb41deb9de9ca549ee
ierceg <ivan@softwaremarbles.com>
2017-02-05 01:23:17 -0300

    engine-docker-builder - Add NPM_TOKEN and metadata labeling
    Fix tagging issues

--- source/tools/engine-docker-builder/index.js
+++ source/tools/engine-docker-builder/index.js
@@ -7,6 +7,7 @@ const _ = require('lodash');
 const Docker = require('node-docker-api').Docker;
 const tar = require('tar-fs');
 const async = require('async');
+const fs = require('fs-extra');
 
 const tags = process.argv.splice(2);
 
@@ -29,17 +30,48 @@ const promisifyStream = stream => new Promise((resolve, reject) => {
 
 const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
-const tarStream = tar.pack('/sources');
-docker.image.build(tarStream, {
-    t: _.head(tags)
-})
-    .then(stream => promisifyStream(stream))
-    .then(() => docker.image.status(_.head(tags)))
-    .then((image) => {
-        async.eachSeries(_.tail(tags), (tag, next) => {
-            image.tag(tag)
-                .then(() => next())
-                .catch(next);
-        });
-    })
-    .catch(console.error);
+fs.readFile('/sources/metadata.json', (err, metadata) => {
+    if (!err && metadata) {
+        // Normalize JSON.
+        try {
+            // lazy ignore no-param-reassign
+            metadata = JSON.stringify(JSON.parse(metadata));
+        } catch (parseErr) {
+            console.error('Invalid metadata:', parseErr);
+            process.exit(-1);
+        }
+    }
+
+    // TODO: Use .dockerignore if it exists in sources to ignore the files during compression.
+    const tarStream = tar.pack('/sources');
+    const buildTag = _.head(tags);
+    const buildParams = {
+        t: buildTag,
+        buildargs: {
+            NPM_TOKEN: process.env.NPM_TOKEN
+        },
+        labels: {
+            'org.getlazy.lazy.engine.metadata': metadata
+        }
+    };
+    docker.image.build(tarStream, buildParams)
+        .then(stream => promisifyStream(stream))
+        .then(() => docker.image.status(buildTag))
+        .then(image => new Promise((resolve, reject) => {
+            async.eachSeries(_.tail(tags), (completeTag, next) => {
+                const [repo, tag] = completeTag.split(':');
+                console.log('tagging image with', { repo, tag });
+                image.tag({ repo, tag })
+                    .then(() => next())
+                    .catch(next);
+            }, (seriesErr) => {
+                if (seriesErr) {
+                    reject(seriesErr);
+                    return;
+                }
+
+                resolve(image);
+            });
+        }))
+        .catch(console.error);
+});
