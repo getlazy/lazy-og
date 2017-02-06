13a70b90e82f395ba36bddb43dc4cd7c0977ba6d
ierceg <ivan@softwaremarbles.com>
2017-02-06 18:56:18 -0300

    php-l-engine - Adapt to helper containers created by lazy

--- source/engines/php-l/php-l-engine.js
+++ source/engines/php-l/php-l-engine.js
@@ -2,23 +2,19 @@
 'use strict';
 
 const _ = require('lodash');
-const H = require('higher');
 const EngineHelpers = require('@lazyass/engine-helpers');
 
 const HelperContainer = EngineHelpers.HelperContainer;
 const EngineHttpServer = EngineHelpers.EngineHttpServer;
 
-const REPOSITORY_AUTH = JSON.parse(
-    H.unless(H.isNonEmptyString, '{}', process.env.LAZY_REPOSITORY_AUTH_JSON));
-
-const LAZY_VOLUME_NAME = process.env.LAZY_VOLUME_NAME;
-
-const HELPER_CONTAINER_IMAGE_NAME = 'php:7.0.13-cli';
-
 //  We are implicitly using `this` in overridden methods but lazy keep telling us not to.
-/* eslint class-methods-use-this: off */
-class PhpLHelperContainer extends HelperContainer
-{
+// lazy ignore class-methods-use-this
+class PhpLHelperContainer extends HelperContainer {
+    constructor() {
+        // Per image-metadata our helper container ID is php-l.
+        super('php-l');
+    }
+
     _getContainerCmd() {
         return ['php', '--syntax-check',
             '--define', 'display_errors=On',
@@ -58,16 +54,10 @@ class PhpLHelperContainer extends HelperContainer
     }
 }
 
-class PhpLEngineHttpServer extends EngineHttpServer
-{
+class PhpLEngineHttpServer extends EngineHttpServer {
     beforeListening() {
-        return HelperContainer
-            .createContainer(REPOSITORY_AUTH, HELPER_CONTAINER_IMAGE_NAME, LAZY_VOLUME_NAME)
-            .then((containerId) => {
-                //  Assume that the container has started correctly.
-                this._containerId = containerId;
-                this._helperContainer = new PhpLHelperContainer(containerId);
-            });
+        this._helperContainer = new PhpLHelperContainer();
+        return Promise.resolve();
     }
 
     getMeta() {
@@ -83,20 +73,11 @@ class PhpLEngineHttpServer extends EngineHttpServer
 
     afterListening() {
         this._helperContainer = null;
-
-        //  Prevent trying to stop the same container twice.
-        if (this._containerId) {
-            const containerId = this._containerId;
-            this._containerId = null;
-            return HelperContainer.deleteContainer(containerId);
-        }
-
         return Promise.resolve();
     }
 }
 
-class Engine
-{
+class Engine {
     start() {
         const port = process.env.PORT || 80;
         this._server = new PhpLEngineHttpServer(port);
