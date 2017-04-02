b8614a605b23b04b88e2b97c80ed155d0a5e74d3
ierceg <ivan@softwaremarbles.com>
2017-04-02 01:52:05 -0300

    eslint-engine-strategy - Convert tabs to spaces

--- source/engines/eslint-engine-strategy/index.js
+++ source/engines/eslint-engine-strategy/index.js
@@ -17,107 +17,107 @@ let availablePlugins;
  *                   and all required module downloaded & installed
  */
 const _configure = (eslintConfiguration) => {
-	return new Promise((resolve, reject) => {
-		const packages = [];
-		const installedPlugins = [];
-		const rules = {};
+    return new Promise((resolve, reject) => {
+        const packages = [];
+        const installedPlugins = [];
+        const rules = {};
 
-		const plugins = _.get(eslintConfiguration, 'plugins', []);
-		if (!_.isEmpty(plugins)) {
-			_.forEach(plugins, (onePlugin) => {
-				const packageName = _.get(onePlugin, 'packageName', '');
-				const packageVersion = _.get(onePlugin, 'packageVersion', 'latest');
-				const pluginName = _.get(onePlugin, 'name', '');
-				packages.push(`${packageName}@${packageVersion}`);
-				if (!_.isEmpty(pluginName)) {
-					installedPlugins.push(pluginName);
-				}
-			});
+        const plugins = _.get(eslintConfiguration, 'plugins', []);
+        if (!_.isEmpty(plugins)) {
+            _.forEach(plugins, (onePlugin) => {
+                const packageName = _.get(onePlugin, 'packageName', '');
+                const packageVersion = _.get(onePlugin, 'packageVersion', 'latest');
+                const pluginName = _.get(onePlugin, 'name', '');
+                packages.push(`${packageName}@${packageVersion}`);
+                if (!_.isEmpty(pluginName)) {
+                    installedPlugins.push(pluginName);
+                }
+            });
 
-			logger.info('Downloading and installing packages:', packages);
-			const spawnSyncResult = yarnInstall(packages);
-			if (spawnSyncResult.status !== 0) {
-				reject(new Error(`yarn failed with ${spawnSyncResult.status}`));
-				return;
-			}
-		}
-		resolve({
-			installedPlugins
-		});
-	});
+            logger.info('Downloading and installing packages:', packages);
+            const spawnSyncResult = yarnInstall(packages);
+            if (spawnSyncResult.status !== 0) {
+                reject(new Error(`yarn failed with ${spawnSyncResult.status}`));
+                return;
+            }
+        }
+        resolve({
+            installedPlugins
+        });
+    });
 };
 
 const _getEslintCli = (localConfig) => {
-	return new CLIEngine({
-		envs: _.get(localConfig, 'env', ['node', 'es6']),
-		parser: _.get(localConfig, 'parser', 'babel-eslint'),
-		plugins: availablePlugins,
-		rules: _.get(localConfig, 'rules', {}),
-		fix: false,
-		parserOptions: _.get(localConfig, 'parserOptions', {
-			ecmaVersion: 7
-		}),
-	});
+    return new CLIEngine({
+        envs: _.get(localConfig, 'env', ['node', 'es6']),
+        parser: _.get(localConfig, 'parser', 'babel-eslint'),
+        plugins: availablePlugins,
+        rules: _.get(localConfig, 'rules', {}),
+        fix: false,
+        parserOptions: _.get(localConfig, 'parserOptions', {
+            ecmaVersion: 7
+        }),
+    });
 };
 
 module.exports = {
 
-	configure: (config) => {
-		return _configure(config)
-			.then((cfg) => {
-				availablePlugins = cfg.installedPlugins;
-			})
-	},
+    configure: (config) => {
+        return _configure(config)
+            .then((cfg) => {
+                availablePlugins = cfg.installedPlugins;
+            })
+    },
 
-	shutDown: () => {
-		return Promise.resolve();
-	},
+    shutDown: () => {
+        return Promise.resolve();
+    },
 
-	handleRequest: (hostPath, language, content, context) => {
+    handleRequest: (hostPath, language, content, context) => {
 
-		const localConfig = _.get(context, 'engineParams.config', {});
+        const localConfig = _.get(context, 'engineParams.config', {});
 
-		return new Promise((resolve) => {
-			const res = _getEslintCli(localConfig).executeOnText(content, hostPath);
-			const results = _.head(_.get(res, 'results'));
-			const messages = _.get(results, 'messages');
+        return new Promise((resolve) => {
+            const res = _getEslintCli(localConfig).executeOnText(content, hostPath);
+            const results = _.head(_.get(res, 'results'));
+            const messages = _.get(results, 'messages');
 
-			const warnings = _
-				.chain(messages)
-				.map((warning) => {
-					const rWarning = {
-						type: _.eq(warning.severity, 2) ? 'Error' : 'Warning',
-						message: `[${warning.ruleId}]: ${warning.message}`,
-						ruleId: warning.ruleId,
-						line: warning.line,
-						column: warning.column
-					};
+            const warnings = _
+                .chain(messages)
+                .map((warning) => {
+                    const rWarning = {
+                        type: _.eq(warning.severity, 2) ? 'Error' : 'Warning',
+                        message: `[${warning.ruleId}]: ${warning.message}`,
+                        ruleId: warning.ruleId,
+                        line: warning.line,
+                        column: warning.column
+                    };
 
-					if (!_.isNull(warning.ruleId)) {
-						const ruleDocs = getRuleURI(warning.ruleId);
-						const moreInfoUrl = (ruleDocs.found) ? ruleDocs.url : `https://www.google.com/search?q=${warning.ruleId}`;
-						rWarning.moreInfo = moreInfoUrl;
-					}
+                    if (!_.isNull(warning.ruleId)) {
+                        const ruleDocs = getRuleURI(warning.ruleId);
+                        const moreInfoUrl = (ruleDocs.found) ? ruleDocs.url : `https://www.google.com/search?q=${warning.ruleId}`;
+                        rWarning.moreInfo = moreInfoUrl;
+                    }
 
           rWarning.fix = _.get(warning, 'fix', {});
-					return rWarning;
-				})
-				.filter()
-				.value();
+                    return rWarning;
+                })
+                .filter()
+                .value();
 
-			resolve({
-				status: {
-					codeChecked: true
-				},
-				warnings
-			});
-		});
-	},
+            resolve({
+                status: {
+                    codeChecked: true
+                },
+                warnings
+            });
+        });
+    },
 
-	getMeta: () => {
-		return {
-			languages: ['JavaScript']
-		};
-	},
+    getMeta: () => {
+        return {
+            languages: ['JavaScript']
+        };
+    },
 
 };
