c720c1e97ca28f68a9a792ce25506b93c4321bcf
ierceg <ivan@softwaremarbles.com>
2017-04-11 22:25:55 -0300

    Fix lazy warnings

--- source/engines/node-strategy-engine/template/engine-sample.js
+++ source/engines/node-strategy-engine/template/engine-sample.js
@@ -1,32 +1,35 @@
-'use strict'
+
+'use strict';
+
+// lazy ignore-all ; this is an example, not production code
 
 module.exports = {
 
-	configure: (config) => {
-		return new Promise((resolve, reject) => {
-			// use config to boot up and get ready.
-			resolve();
-		});
-	},
+    configure: (config) => {
+        return new Promise((resolve, reject) => {
+            // use config to boot up and get ready.
+            resolve();
+        });
+    },
 
-	shutDown: () => {
-		return new Promise((resolve, reject) => {
-			// cleanup
-			resolve();
-		});
-	},
+    shutDown: () => {
+        return new Promise((resolve, reject) => {
+            // cleanup
+            resolve();
+        });
+    },
 
-	handleRequest: (hostPath, language, content, context) => {
-		return new Promise((resolve, reject) => {
-			// do the job
-			resolve();
-		});
-	},
+    handleRequest: (hostPath, language, content, context) => {
+        return new Promise((resolve, reject) => {
+            // do the job
+            resolve();
+        });
+    },
 
-	getMeta: () => {
-		return {
-			languages: ['JavaScript']
-		};
-	}
+    getMeta: () => {
+        return {
+            languages: ['JavaScript']
+        };
+    }
 
 };
