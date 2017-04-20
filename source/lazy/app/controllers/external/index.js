0fc03c8a4d7212394e1b629b9b825e2d41758489
ierceg <ivan@softwaremarbles.com>
2017-04-20 00:08:49 -0300

    (lazy) Fix lazy warnings

--- source/lazy/app/controllers/external/index.js
+++ source/lazy/app/controllers/external/index.js
@@ -1,10 +1,12 @@
 
 'use strict';
 
+// lazy ignore-once arrow-body-style
 const initialize = (app, options) => {
+    // lazy ignore-once global-require
     return require('./engine-controller').initialize(app, options);
 };
 
 module.exports = {
-    initialize: initialize
+    initialize
 };
