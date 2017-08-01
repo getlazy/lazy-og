05ba91c44363648c8b023db9422238942f7f4b44
ierceg <ivan@softwaremarbles.com>
2017-07-31 23:53:08 -0400

    (govet) Add JSON logging

--- source/engines/govet/govet-engine.go
+++ source/engines/govet/govet-engine.go
@@ -11,14 +11,35 @@ type Metadata struct {
 	Languages []string `json:"languages"`
 }
 
+type logEntry struct {
+	Level string `json:"level"`
+	Message string `json:"message"`
+	Meta map[string]string `json:"meta"`
+}
+
+func logWithMetadata(level, message string, metadata map[string]string) {
+	entry := &logEntry{level, message, metadata}
+	marshaledEntry, err := json.Marshal(entry)
+	if err != nil {
+		logWithMetadata("error", "Failed to marshal log entry", map[string]string{"error": err.Error()})
+		return
+	}
+
+	fmt.Println(string(marshaledEntry))
+}
+
+func log(level, message string) {
+	logWithMetadata(level, message, nil)
+}
+
 func getStatusHandler(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Println("{\"message\":\"GET /status\"}")
+	log("info", "GET /status")
 	responseWriter.WriteHeader(http.StatusOK)
 }
 
 func getMetaHandler(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Println("{\"message\":\"GET /meta\"}")
 	meta := &Metadata{[]string{"go"}}
+	log("info", "GET /meta")
 	marshaledMeta, err := json.Marshal(meta)
 	if err != nil {
 		responseWriter.WriteHeader(http.StatusInternalServerError)
@@ -29,7 +50,7 @@ func getMetaHandler(responseWriter http.ResponseWriter, request *http.Request) {
 }
 
 func postFileHandler(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Println("{\"message\":\"POST /file\"}")
+	log("info", "POST /file")
 	responseWriter.WriteHeader(http.StatusOK)
 }
 
@@ -37,6 +58,6 @@ func main() {
 	http.HandleFunc("/status", getStatusHandler)
 	http.HandleFunc("/meta", getMetaHandler)
 	http.HandleFunc("/file", postFileHandler)
-	fmt.Println("{\"message\":\"Engine started.\"}")
+	log("info", "Engine started.")
 	http.ListenAndServe(":80", nil)
 }
